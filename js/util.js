a48c7f6707d713c7b2dba2b97db66939902af7da
themattayahu <119358623+themattayahu@users.noreply.github.com>
2023-01-23 04:23:13 +0000

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,27 +1,5 @@
-var loadLogs = (username, from, to) => {
-  let logs = null;
-  let formatted_logs = [];
-  $.ajax({
-    url: "https://surskity.live/logs",
-    type: "get",
-    async: false,
-    data: {
-      from: from,
-      to: to,
-    },
-    success: (data) => {
-      logs = data;
-    },
+var loadLogs = (from, to) => {
+  return fetch("https://surskity.live/logs?from=" + from + "&to=" + to, {
+    mode: "cors",
   });
-  for (const [key, value] of Object.entries(logs)) {
-    value.forEach((e) => {
-      if (e.username == username) {
-        var datetime = new Date(Number(key) * 1000).toLocaleString("en-US");
-        formatted_logs.push(
-          "[" + datetime + "] " + e.username + ": " + e.message
-        );
-      }
-    });
-  }
-  return formatted_logs;
 };
